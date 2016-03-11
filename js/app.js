438489b508d646a77f019a8021acabe2815e5eb1
loraruffin <lora.ruffin@colorado.edu>
2016-03-10 18:47:11 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+angular.module('interview', ['ngRoute', 'cityControllers']) //'ngRoute', 'ngResource'
+.factory('dataFactory', ['$http' , function($http){
+    var factory = {};
+    factory.getData = function (url) {
+        return $http.get(url);
+    };
+
+    return factory;
+
+
+
+}])
+
+.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/city', {
+        templateUrl: 'partials/city-list.html',
+        controller: 'CityListCtrl'
+      }).
+      when('/city/:cityId', {
+        templateUrl: 'partials/thumb-list.html',
+        controller: 'ThumbListCtrl'
+      }).
+      otherwise({
+        redirectTo: '/city'
+      });
+  }]);
---
+++ js/controllers.js
@@ -0,0 +1,25 @@
+var cityControllers = angular.module('cityControllers', []);
+
+
+
+
+cityControllers.controller('CityListCtrl',[ '$scope', '$http', 'dataFactory', function($scope, $http, dataFactory) {
+
+    //$http.get('http://mqlocal.aol.com/photos').then(function(cityData){
+    dataFactory.getData('http://mqlocal.aol.com/photos').then(function(cityData){
+        $scope.cities = cityData.data;
+    });
+
+}]);
+
+
+cityControllers.controller('ThumbListCtrl',[ '$scope', '$http', '$routeParams', 'dataFactory', function($scope, $http, $routeParams, dataFactory) {
+
+    $scope.cityId = $routeParams.cityId;
+    $scope.url = 'http://mqlocal.aol.com/photos/' +  $scope.cityId;
+
+    dataFactory.getData($scope.url).then(function(imagesData){
+        $scope.photos = imagesData.data.photos;
+        });
+
+}]);
